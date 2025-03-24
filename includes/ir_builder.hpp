33b7ef603c5455f0d65f2a91633db5f7247c59f8
Aleksandr Mishin <mishin.aa@phystech.edu>
2025-03-24 17:48:41 +0300

    ir_builder split
    Signed-off-by: Aleksandr Mishin <mishin.aa@phystech.edu>

--- includes/ir_builder.hpp
+++ includes/ir_builder.hpp
@@ -13,210 +13,60 @@ namespace custom {
 
 class IRBuilder final {
  public:
-  static Function* createFunction(std::string name,
-                                  Type rettype,
-                                  std::vector<Instruction*> params) {
-    return new Function(name, rettype, params);
-  }
+  ~IRBuilder();
 
-  static Graph* createGraph() {
-    return new Graph();
-  }
+  Function* createFunction(std::string name, Type rettype, std::vector<Instruction*> params);
 
-  static BasicBlock* createBasicBlock(Graph* graph) {
-    BasicBlock* basic_block = new BasicBlock(graph);
-    graph->addBasicBlock(basic_block);
-    return basic_block;
-  }
+  Graph* createGraph();
 
-  static Instruction* createMOV(Type type, BasicBlock* basic_block, ImmType imm) {
-    Instruction* instr = new MovInstruction(Opcode::MOV, type, basic_block, imm);
-    basic_block->pushback_instr(instr);
-    return instr;
-  }
+  BasicBlock* createBasicBlock(Graph* graph);
 
-  static Instruction* createMOVI(Type type, BasicBlock* basic_block, ImmType imm) {
-    Instruction* instr = new MovInstruction(Opcode::MOVI, type, basic_block, imm);
-    basic_block->pushback_instr(instr);
-    return instr;
-  }
-
-  static Instruction* createRET(Type type, BasicBlock* basic_block, Instruction* retinstr) {
-    Instruction* instr = new RetInstruction(Opcode::RET, type, basic_block, retinstr);
-    basic_block->pushback_instr(instr);
-    return instr;
-  }
-
-  static Instruction* createRET(Type type, BasicBlock* basic_block) {
-    Instruction* instr = new RetInstruction(Opcode::RET, type, basic_block);
-    basic_block->pushback_instr(instr);
-    return instr;
-  }
-
-  static Instruction* createRETI(Type type, BasicBlock* basic_block, ImmType imm) {
-    Instruction* instr = new RetInstruction(Opcode::RETI, type, basic_block, imm);
-    basic_block->pushback_instr(instr);
-    return instr;
-  }
-
-  static Instruction* createCAST(Type type, BasicBlock* basic_block, Instruction* castinstr) {
-    Instruction* instr = new CastInstruction(Opcode::CAST, type, basic_block, castinstr);
-    basic_block->pushback_instr(instr);
-    return instr;
-  }
-
-  static Instruction* createJUMP(BasicBlock* basic_block, BasicBlock* trueBasicBlock) {
-    Instruction* instr =
-        new JumpInstruction(Opcode::JMP, Type::myvoid, basic_block, trueBasicBlock);
-    basic_block->pushback_instr(instr);
-    return instr;
-  }
-
-  static Instruction* createJA(BasicBlock* basic_block,
-                               BasicBlock* trueBasicBlock,
-                               BasicBlock* falseBasicBlock) {
-    Instruction* instr =
-        new JumpInstruction(Opcode::JA, Type::myvoid, basic_block, trueBasicBlock, falseBasicBlock);
-    basic_block->pushback_instr(instr);
-    return instr;
-  }
-
-  static Instruction* createADD(Type type,
-                                BasicBlock* basic_block,
-                                Instruction* first,
-                                Instruction* second) {
-    Instruction* instr = new InstructionFrom2Instr(Opcode::ADD, type, basic_block, first, second);
-    basic_block->pushback_instr(instr);
-    return instr;
-  }
-
-  static Instruction* createADDI(Type type,
-                                 BasicBlock* basic_block,
-                                 Instruction* first,
-                                 ImmType imm) {
-    Instruction* instr =
-        new InstructionFromInstrAndImm(Opcode::ADDI, type, basic_block, first, imm);
-    basic_block->pushback_instr(instr);
-    return instr;
-  }
-
-  static Instruction* createSUB(Type type,
-                                BasicBlock* basic_block,
-                                Instruction* first,
-                                Instruction* second) {
-    Instruction* instr = new InstructionFrom2Instr(Opcode::SUB, type, basic_block, first, second);
-    basic_block->pushback_instr(instr);
-    return instr;
-  }
-
-  static Instruction* createSUBI(Type type,
-                                 BasicBlock* basic_block,
-                                 Instruction* first,
-                                 ImmType imm) {
-    Instruction* instr =
-        new InstructionFromInstrAndImm(Opcode::SUBI, type, basic_block, first, imm);
-    basic_block->pushback_instr(instr);
-    return instr;
-  }
-
-  static Instruction* createMUL(Type type,
-                                BasicBlock* basic_block,
-                                Instruction* first,
-                                Instruction* second) {
-    Instruction* instr = new InstructionFrom2Instr(Opcode::MUL, type, basic_block, first, second);
-    basic_block->pushback_instr(instr);
-    return instr;
-  }
-
-  static Instruction* createMULI(Type type,
-                                 BasicBlock* basic_block,
-                                 Instruction* first,
-                                 ImmType imm) {
-    Instruction* instr =
-        new InstructionFromInstrAndImm(Opcode::MULI, type, basic_block, first, imm);
-    basic_block->pushback_instr(instr);
-    return instr;
-  }
-
-  static Instruction* createASHR(Type type,
-                                 BasicBlock* basic_block,
-                                 Instruction* first,
-                                 Instruction* second) {
-    Instruction* instr = new InstructionFrom2Instr(Opcode::ASHR, type, basic_block, first, second);
-    basic_block->pushback_instr(instr);
-    return instr;
-  }
-
-  static Instruction* createASHRI(Type type,
-                                  BasicBlock* basic_block,
-                                  Instruction* first,
-                                  ImmType imm) {
-    Instruction* instr =
-        new InstructionFromInstrAndImm(Opcode::ASHRI, type, basic_block, first, imm);
-    basic_block->pushback_instr(instr);
-    return instr;
-  }
-
-  static Instruction* createXOR(Type type,
-                                BasicBlock* basic_block,
-                                Instruction* first,
-                                Instruction* second) {
-    Instruction* instr = new InstructionFrom2Instr(Opcode::XOR, type, basic_block, first, second);
-    basic_block->pushback_instr(instr);
-    return instr;
-  }
-
-  static Instruction* createXORI(Type type,
-                                 BasicBlock* basic_block,
-                                 Instruction* first,
-                                 ImmType imm) {
-    Instruction* instr =
-        new InstructionFromInstrAndImm(Opcode::XORI, type, basic_block, first, imm);
-    basic_block->pushback_instr(instr);
-    return instr;
-  }
-
-  static Instruction* createCMP(Type type,
-                                BasicBlock* basic_block,
-                                Instruction* first,
-                                Instruction* second) {
-    Instruction* instr = new InstructionFrom2Instr(Opcode::CMP, type, basic_block, first, second);
-    basic_block->pushback_instr(instr);
-    return instr;
-  }
-
-  static Instruction* createNEQ(Type type,
-                                BasicBlock* basic_block,
-                                Instruction* first,
-                                Instruction* second) {
-    Instruction* instr = new InstructionFrom2Instr(Opcode::NEQ, type, basic_block, first, second);
-    basic_block->pushback_instr(instr);
-    return instr;
-  }
-
-  static Instruction* createNEQ(Type type, BasicBlock* basic_block, Instruction* first) {
-    Instruction* instr = new NeqInstruction(Opcode::NEQ, type, basic_block, first);
-    basic_block->pushback_instr(instr);
-    return instr;
-  }
-
-  static Instruction* createPHI(Type type, BasicBlock* basic_block) {
-    Instruction* instr = new PhiInstruction(Opcode::PHI, type, basic_block);
-    basic_block->pushback_instr(instr);
-    return instr;
-  }
-
-  Instruction* createPARAM(Type type) {
-    Instruction* instr = new Param(Opcode::PARAM, type);
-    _params.push_back(instr);
-    return instr;
-  }
-
-  ~IRBuilder() {
-    for (auto& tmp_param : _params) {
-      delete tmp_param;
-    }
-  }
+  Instruction* createMOV(Type type, BasicBlock* basic_block, ImmType imm);
+  Instruction* createMOVI(Type type, BasicBlock* basic_block, ImmType imm);
+  Instruction* createRET(Type type, BasicBlock* basic_block, Instruction* retinstr);
+  Instruction* createRET(Type type, BasicBlock* basic_block);
+  Instruction* createRETI(Type type, BasicBlock* basic_block, ImmType imm);
+  Instruction* createCAST(Type type, BasicBlock* basic_block, Instruction* castinstr);
+  Instruction* createJUMP(BasicBlock* basic_block, BasicBlock* trueBasicBlock);
+  Instruction* createJA(BasicBlock* basic_block,
+                        BasicBlock* trueBasicBlock,
+                        BasicBlock* falseBasicBlock);
+  Instruction* createADD(Type type,
+                         BasicBlock* basic_block,
+                         Instruction* first,
+                         Instruction* second);
+  Instruction* createADDI(Type type, BasicBlock* basic_block, Instruction* first, ImmType imm);
+  Instruction* createSUB(Type type,
+                         BasicBlock* basic_block,
+                         Instruction* first,
+                         Instruction* second);
+  Instruction* createSUBI(Type type, BasicBlock* basic_block, Instruction* first, ImmType imm);
+  Instruction* createMUL(Type type,
+                         BasicBlock* basic_block,
+                         Instruction* first,
+                         Instruction* second);
+  Instruction* createMULI(Type type, BasicBlock* basic_block, Instruction* first, ImmType imm);
+  Instruction* createASHR(Type type,
+                          BasicBlock* basic_block,
+                          Instruction* first,
+                          Instruction* second);
+  Instruction* createASHRI(Type type, BasicBlock* basic_block, Instruction* first, ImmType imm);
+  Instruction* createXOR(Type type,
+                         BasicBlock* basic_block,
+                         Instruction* first,
+                         Instruction* second);
+  Instruction* createXORI(Type type, BasicBlock* basic_block, Instruction* first, ImmType imm);
+  Instruction* createCMP(Type type,
+                         BasicBlock* basic_block,
+                         Instruction* first,
+                         Instruction* second);
+  Instruction* createNEQ(Type type,
+                         BasicBlock* basic_block,
+                         Instruction* first,
+                         Instruction* second);
+  Instruction* createNEQ(Type type, BasicBlock* basic_block, Instruction* first);
+  Instruction* createPHI(Type type, BasicBlock* basic_block);
+  Instruction* createPARAM(Type type);
 
  private:
   std::vector<Instruction*> _params;
---
+++ src/ir_builder.cpp
@@ -0,0 +1,204 @@
+#include "ir_builder.hpp"
+
+namespace custom {
+
+Function* IRBuilder::createFunction(std::string name,
+                                    Type rettype,
+                                    std::vector<Instruction*> params) {
+  return new Function(name, rettype, params);
+}
+
+Graph* IRBuilder::createGraph() {
+  return new Graph();
+}
+
+BasicBlock* IRBuilder::createBasicBlock(Graph* graph) {
+  BasicBlock* basic_block = new BasicBlock(graph);
+  graph->addBasicBlock(basic_block);
+  return basic_block;
+}
+
+Instruction* IRBuilder::createMOV(Type type, BasicBlock* basic_block, ImmType imm) {
+  Instruction* instr = new MovInstruction(Opcode::MOV, type, basic_block, imm);
+  basic_block->pushback_instr(instr);
+  return instr;
+}
+
+Instruction* IRBuilder::createMOVI(Type type, BasicBlock* basic_block, ImmType imm) {
+  Instruction* instr = new MovInstruction(Opcode::MOVI, type, basic_block, imm);
+  basic_block->pushback_instr(instr);
+  return instr;
+}
+
+Instruction* IRBuilder::createRET(Type type, BasicBlock* basic_block, Instruction* retinstr) {
+  Instruction* instr = new RetInstruction(Opcode::RET, type, basic_block, retinstr);
+  basic_block->pushback_instr(instr);
+  return instr;
+}
+
+Instruction* IRBuilder::createRET(Type type, BasicBlock* basic_block) {
+  Instruction* instr = new RetInstruction(Opcode::RET, type, basic_block);
+  basic_block->pushback_instr(instr);
+  return instr;
+}
+
+Instruction* IRBuilder::createRETI(Type type, BasicBlock* basic_block, ImmType imm) {
+  Instruction* instr = new RetInstruction(Opcode::RETI, type, basic_block, imm);
+  basic_block->pushback_instr(instr);
+  return instr;
+}
+
+Instruction* IRBuilder::createCAST(Type type, BasicBlock* basic_block, Instruction* castinstr) {
+  Instruction* instr = new CastInstruction(Opcode::CAST, type, basic_block, castinstr);
+  basic_block->pushback_instr(instr);
+  return instr;
+}
+
+Instruction* IRBuilder::createJUMP(BasicBlock* basic_block, BasicBlock* trueBasicBlock) {
+  Instruction* instr = new JumpInstruction(Opcode::JMP, Type::myvoid, basic_block, trueBasicBlock);
+  basic_block->pushback_instr(instr);
+  return instr;
+}
+
+Instruction* IRBuilder::createJA(BasicBlock* basic_block,
+                                 BasicBlock* trueBasicBlock,
+                                 BasicBlock* falseBasicBlock) {
+  Instruction* instr =
+      new JumpInstruction(Opcode::JA, Type::myvoid, basic_block, trueBasicBlock, falseBasicBlock);
+  basic_block->pushback_instr(instr);
+  return instr;
+}
+
+Instruction* IRBuilder::createADD(Type type,
+                                  BasicBlock* basic_block,
+                                  Instruction* first,
+                                  Instruction* second) {
+  Instruction* instr = new InstructionFrom2Instr(Opcode::ADD, type, basic_block, first, second);
+  basic_block->pushback_instr(instr);
+  return instr;
+}
+
+Instruction* IRBuilder::createADDI(Type type,
+                                   BasicBlock* basic_block,
+                                   Instruction* first,
+                                   ImmType imm) {
+  Instruction* instr = new InstructionFromInstrAndImm(Opcode::ADDI, type, basic_block, first, imm);
+  basic_block->pushback_instr(instr);
+  return instr;
+}
+
+Instruction* IRBuilder::createSUB(Type type,
+                                  BasicBlock* basic_block,
+                                  Instruction* first,
+                                  Instruction* second) {
+  Instruction* instr = new InstructionFrom2Instr(Opcode::SUB, type, basic_block, first, second);
+  basic_block->pushback_instr(instr);
+  return instr;
+}
+
+Instruction* IRBuilder::createSUBI(Type type,
+                                   BasicBlock* basic_block,
+                                   Instruction* first,
+                                   ImmType imm) {
+  Instruction* instr = new InstructionFromInstrAndImm(Opcode::SUBI, type, basic_block, first, imm);
+  basic_block->pushback_instr(instr);
+  return instr;
+}
+
+Instruction* IRBuilder::createMUL(Type type,
+                                  BasicBlock* basic_block,
+                                  Instruction* first,
+                                  Instruction* second) {
+  Instruction* instr = new InstructionFrom2Instr(Opcode::MUL, type, basic_block, first, second);
+  basic_block->pushback_instr(instr);
+  return instr;
+}
+
+Instruction* IRBuilder::createMULI(Type type,
+                                   BasicBlock* basic_block,
+                                   Instruction* first,
+                                   ImmType imm) {
+  Instruction* instr = new InstructionFromInstrAndImm(Opcode::MULI, type, basic_block, first, imm);
+  basic_block->pushback_instr(instr);
+  return instr;
+}
+
+Instruction* IRBuilder::createASHR(Type type,
+                                   BasicBlock* basic_block,
+                                   Instruction* first,
+                                   Instruction* second) {
+  Instruction* instr = new InstructionFrom2Instr(Opcode::ASHR, type, basic_block, first, second);
+  basic_block->pushback_instr(instr);
+  return instr;
+}
+
+Instruction* IRBuilder::createASHRI(Type type,
+                                    BasicBlock* basic_block,
+                                    Instruction* first,
+                                    ImmType imm) {
+  Instruction* instr = new InstructionFromInstrAndImm(Opcode::ASHRI, type, basic_block, first, imm);
+  basic_block->pushback_instr(instr);
+  return instr;
+}
+
+Instruction* IRBuilder::createXOR(Type type,
+                                  BasicBlock* basic_block,
+                                  Instruction* first,
+                                  Instruction* second) {
+  Instruction* instr = new InstructionFrom2Instr(Opcode::XOR, type, basic_block, first, second);
+  basic_block->pushback_instr(instr);
+  return instr;
+}
+
+Instruction* IRBuilder::createXORI(Type type,
+                                   BasicBlock* basic_block,
+                                   Instruction* first,
+                                   ImmType imm) {
+  Instruction* instr = new InstructionFromInstrAndImm(Opcode::XORI, type, basic_block, first, imm);
+  basic_block->pushback_instr(instr);
+  return instr;
+}
+
+Instruction* IRBuilder::createCMP(Type type,
+                                  BasicBlock* basic_block,
+                                  Instruction* first,
+                                  Instruction* second) {
+  Instruction* instr = new InstructionFrom2Instr(Opcode::CMP, type, basic_block, first, second);
+  basic_block->pushback_instr(instr);
+  return instr;
+}
+
+Instruction* IRBuilder::createNEQ(Type type,
+                                  BasicBlock* basic_block,
+                                  Instruction* first,
+                                  Instruction* second) {
+  Instruction* instr = new InstructionFrom2Instr(Opcode::NEQ, type, basic_block, first, second);
+  basic_block->pushback_instr(instr);
+  return instr;
+}
+
+Instruction* IRBuilder::createNEQ(Type type, BasicBlock* basic_block, Instruction* first) {
+  Instruction* instr = new NeqInstruction(Opcode::NEQ, type, basic_block, first);
+  basic_block->pushback_instr(instr);
+  return instr;
+}
+
+Instruction* IRBuilder::createPHI(Type type, BasicBlock* basic_block) {
+  Instruction* instr = new PhiInstruction(Opcode::PHI, type, basic_block);
+  basic_block->pushback_instr(instr);
+  return instr;
+}
+
+Instruction* IRBuilder::createPARAM(Type type) {
+  Instruction* instr = new Param(Opcode::PARAM, type);
+  _params.push_back(instr);
+  return instr;
+}
+
+IRBuilder::~IRBuilder() {
+  for (auto& tmp_param : _params) {
+    delete tmp_param;
+  }
+}
+
+}  // namespace custom
